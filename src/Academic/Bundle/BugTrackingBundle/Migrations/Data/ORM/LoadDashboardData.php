fc726cdcc019c7bbaf14243fe1e9ffa0de418c0e
forban <forban@pitechnologies.ro>
2016-10-12 18:10:38 +0300

    INTAP-101: Dashboard issues grid - added dashboard fixtures for new widget - added recent issue widget template - translations - dashboard widget config - datagrid for recent issues

---
+++ src/Academic/Bundle/BugTrackingBundle/Migrations/Data/ORM/LoadDashboardData.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Academic\Bundle\BugTrackingBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Oro\Bundle\DashboardBundle\Migrations\Data\ORM\AbstractDashboardFixture;
+use Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData;
+use Oro\Bundle\DashboardBundle\Migrations\Data\ORM\LoadDashboardData as DashboardData;
+
+class LoadDashboardData extends AbstractDashboardFixture implements DependentFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return [
+            LoadAdminUserData::class,
+            DashboardData::class,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $dashboard = $this->findAdminDashboardModel(
+            $manager,
+            'main'
+        );
+
+        $dashboard
+            ->setIsDefault(true)
+            ->setLabel(
+                $this->container->get('translator')->trans('oro.dashboard.title.main')
+            )
+            ->addWidget(
+                $this->createWidgetModel(
+                    'recent_issues',
+                    [
+                        0,
+                        10
+                    ]
+                )
+            );
+
+        $manager->flush();
+    }
+}
